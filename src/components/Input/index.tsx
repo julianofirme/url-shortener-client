6cdbbed41de4edc29e913643c4bdb22262c4b663
Juliano Firme <julianofirme1@gmail.com>
2022-04-25 22:22:19 -0300

    project setup

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+interface InputProps {
+  
+}
+
+function Input({  }: InputProps) {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default Input;
---
+++ src/components/SearchButton/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+interface SearchButtonProps {
+  
+}
+
+function SearchButton({  }: SearchButtonProps) {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default SearchButton;
