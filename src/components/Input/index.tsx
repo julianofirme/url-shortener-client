78456ca6ff6108a89247a5089b88d5c098f0a967
Juliano Firme <julianofirme1@gmail.com>
2022-04-26 00:14:30 -0300

    feat: input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,23 @@
-import { ReactNode } from 'react';
+import React from 'react';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Box from '@mui/material/Box';
 
 interface InputProps {
-  
+  placeholder: string;
+  onChange: (event: any) => void
 }
 
-function Input({  }: InputProps) {
+function Input({ placeholder, onChange }: InputProps) {
   return (
-    <>
-      
-    </>
+    <Box component="form" noValidate autoComplete="off">
+      <FormControl sx={{ width: '60ch' }}>
+        <OutlinedInput sx={{
+          backgroundColor: 'white',
+          borderRadius: '20px'
+        }} placeholder={placeholder} onChange={onChange} />
+      </FormControl>
+    </Box>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import { Box, Container } from '@mui/material';
+import React, { useContext } from 'react';
+import Input from '../components/Input';
+import { UrlContext } from '../providers/urlProvider';
+
+function Home() {
+  const { setOriginalUrl, url } = useContext(UrlContext)
+
+
+  const handleChange = (event: any) => {
+    const {
+      target: { value }
+    } = event
+
+    setOriginalUrl(value)
+  }
+
+  return (
+    <Container>
+      <h1 style={{ textAlign: 'center', fontSize: '56px' }}>
+        Shortener your <span style={{ color: '#F37D63' }}>links</span>
+        <br />
+        with one click!
+      </h1>
+
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Input placeholder={'Paste your link'} onChange={handleChange} />
+      </Box>
+    </Container>
+  );
+}
+
+export default Home;
